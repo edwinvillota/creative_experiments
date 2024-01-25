545dba7cb3f6511728deb3b505ef46275127ae0f
edwinvillota <edwinvillota@hotmail.com>
2024-01-24 21:53:51 -0500

    Working on deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/creative_experiments/',
   plugins: [react()],
   resolve: {
     alias: {
