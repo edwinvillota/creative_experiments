339683b4ab067560229330eb0f002f0724ecca64
edwinvillota <edwinvillota@hotmail.com>
2026-02-06 23:07:22 -0500

    Added galaxy

--- src/components/organisms/Galaxy/Galaxy.tsx
+++ src/components/organisms/Galaxy/Galaxy.tsx
@@ -1,16 +1,22 @@
+import { useMemo } from 'react';
+
+import { Mouse } from '@/common/classes';
+import type { ICanvasDraw, ICanvasSetup } from '@/common/hooks';
 import { Canvas } from '@/components/molecules';
 
 import { Effect } from './sketch';
 
 export const Galaxy = () => {
   let effect: Effect;
+  const mouse = useMemo(() => new Mouse(), []);
 
-  const setup = (ctx: CanvasRenderingContext2D) => {
+  const setup: ICanvasSetup = (ctx) => {
     effect = new Effect(ctx);
+    effect.mouse = mouse;
     effect.init();
   };
 
-  const draw = () => {
+  const draw: ICanvasDraw = () => {
     effect.render();
   };
 
@@ -18,7 +24,7 @@ export const Galaxy = () => {
     <section className="relative h-full w-full">
       <Canvas
         sketch={{
-          animated: false,
+          animated: true,
           setup,
           draw,
         }}
@@ -26,7 +32,13 @@ export const Galaxy = () => {
           className:
             'w-full h-full absolute top-0 left-0 overflow-hidden bg-black',
         }}
+        canvasProps={{ ...mouse.eventHandlers }}
       />
+      <div className="align-center pointer-events-none relative flex h-full w-full items-center justify-center bg-transparent">
+        <h1 className="text-7xl uppercase text-white opacity-20 mix-blend-overlay md:text-9xl">
+          Galaxy
+        </h1>
+      </div>
     </section>
   );
 };
